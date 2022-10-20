4d89592d72753779109f939f183bebc60224d7a7
Mingri159 <1593683331@qq.com>
2022-10-20 16:52:32 +0800

    调整了一些规则

--- js/rules.js
+++ js/rules.js
@@ -4,14 +4,12 @@ var ruleArray = [
   "-g-n-i+e",
   "-g-n-i-%2-%s-%1$$;",
   "-g-n-i-y+i+e;",
-  "-s-g-n-i-%2$+e", //lodgings -> lodge
-  "-s-g-n-i", //meanderings -> meander
-
+  "-s-g-n-i-%2$+e",
+  "-s-g-n-i",
   "-d-e$",
   "-d-e",
   "-d-e-%2-%s-%1$$",
-
-  "-s", /////////////pl_for_a_noun,
+  "-s",
   "-s-e-s$",
   "-s-e-c$",
   "-s-e-x$",
@@ -19,15 +17,11 @@ var ruleArray = [
   "-s-e-h$",
   "-s-e-i+y",
   "-s-e-v+f",
-
   "-s-e-v+f+e",
-
-  "-y-l", //likely -> like
-  "-y-l-i----$$$$+y", // luckily -> lucky
-  "-y-l-a-c-i$$", // dramatically -> dramatic
-  // "-y-l$+e", // gently-> gentle      错误：supply --> supple
-
-  "-r-e$", //__bijiaoji
+  "-y-l",
+  "-y-l-i----$$$$+y",
+  "-y-l-a-c-i$$",
+  "-r-e$",
   "-r-e",
   "-r-e-i+y",
   "-r-e-%2-%s-%1$$",
@@ -35,23 +29,8 @@ var ruleArray = [
   "-t-s-e",
   "-t-s-e-i+y",
   "-t-s-e-%2-%s-%1$$",
-
-  "-s-r-e$", // workers->work
+  "-s-r-e$",
   "-s-r-e",
   "-s-r-e-i+y",
   "-s-r-e-%2-%2-%1$$",
-
-  "-c-i-%2$", //prophetic -> prophet
-  "-n-o-i-%2$", // invention -> invent
-  "-n-o-i-%2-a-%2$+e", // limitation -> limite
-  "-n-o-i-t-i-%2$+e", // competition -> compete
-
-  "-l-a-c$", // historical->historic
-  "-l-a-c$+e", // practical->practice
-  "-y-t-i-%2$", // originality->original
-  "-e-v-i-t-----$$$$$$", // subjective -> subject
-  "-e-v-i-t$+e-----$$$$$", // generative -> generate
-  "-e-v-i-t-i-%2%+e", // competitive -> compete
-  "-s-s-e-n----$$$$", // plainness -> plain
-  "-t-n-e-m", // government -> govern
 ];
